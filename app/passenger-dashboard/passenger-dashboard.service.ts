f2b3f676bdba7d036427ec77c25e4df59aa16775
Wheezy123 <JohnsonJeromeC@gmail.com>
2018-09-03 18:40:42 -0500

    import rxjs catch & throw, add error handling to http calls

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -4,7 +4,15 @@ import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Passenger } from './models/passenger.interface';
 
 import { Observable } from 'rxjs/Observable';
+// rxjs map operator
 import 'rxjs/add/operator/map';
+
+// rxjs request error handling
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
+// rxjs promise operator
+// import 'rxjs/add/operator/promise';
 
 const PASSENGER_API: string = '/api/passengers';
 
@@ -15,14 +23,29 @@ export class PassengerDashboardService {
     // console.log(this.http);
   }
 
+  // Observable using rxjs map
   getPassengers(): Observable<Passenger[]>{
     return this.http
       .get(PASSENGER_API)
       .map((response: Response) => {
         return response.json();
-      });
+      })
+      // error handling below
+      .catch((error: any) => Observable.throw(error.json()));
   }
 
+  // // *Promise using rxjs promise
+  // getPassengers(): Promise<Passenger[]>{
+  //   return this.http
+  //     .get(PASSENGER_API)
+  //     .toPromise()
+  //     .then((response: Response) => {
+  //       return response.json();
+  //     });
+  // }
+
+
+  // Observable using rxjs map
   updatePassenger(passenger: Passenger): Observable<Passenger>{
     // create custom http headers here, ** must import Headers from angular/http
     let headers = new Headers({
@@ -39,14 +62,48 @@ export class PassengerDashboardService {
       .put(`${PASSENGER_API}/${passenger.id}`, passenger, options)
       .map((response: Response) => {
         return response.json();
-      });
+      })
+      .catch((error: any) => Observable.throw(error.json()));
   }
 
+  // *Promise using rxjs promise
+  // updatePassenger(passenger: Passenger): Promise<Passenger>{
+  //   // create custom http headers here, ** must import Headers from angular/http
+  //   let headers = new Headers({
+  //     'Content-Type': 'application/json'
+  //   });
+
+  //   // create custom request options here, ** must import RequestOptions from angular/http 
+  //   let options = new RequestOptions({
+  //     // referring to headers defined above
+  //     headers: headers
+  //   });
+
+  //   return this.http
+  //     .put(`${PASSENGER_API}/${passenger.id}`, passenger, options)
+  //     .toPromise()
+  //     .then((response: Response) => {
+  //       return response.json();
+  //     });
+  // }
+
+  // Observable using rxjs map
   removePassenger(passenger: Passenger): Observable<Passenger>{
     return this.http
       .delete(`${PASSENGER_API}/${passenger.id}`)
       .map((response: Response) => {
         return response.json();
-      });
+      })
+      .catch((error: any) => Observable.throw(error.json()));
   }
+
+  // *Promise using rxjs promise
+//   removePassenger(passenger: Passenger): Promise<Passenger>{
+//     return this.http
+//       .delete(`${PASSENGER_API}/${passenger.id}`)
+//       .toPromise()
+//       .then((response: Response) => {
+//         return response.json();
+//       });
+//   }
 }
