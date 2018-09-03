dd4df5393c1dc267641c29d5142cfed7e65ef0ae
Wheezy123 <JohnsonJeromeC@gmail.com>
2018-09-03 16:55:07 -0500

    add import passenger dash service, add to providers list

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -8,6 +8,9 @@ import { PassengerDashboardComponent } from './containers/passenger-dashboard/pa
 import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
 import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
 
+// Services
+import { PassengerDashboardService } from './passenger-dashboard.service';
+
 
 @NgModule({
   declarations: [
@@ -20,6 +23,9 @@ import { PassengerDetailComponent } from './components/passenger-detail/passenge
   ],
   exports: [
     PassengerDashboardComponent
+  ],
+  providers: [
+    PassengerDashboardService
   ]
 })
 
