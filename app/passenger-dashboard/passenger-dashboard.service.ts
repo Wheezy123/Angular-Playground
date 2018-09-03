28d7808c600ad26e9a0c1896a227bff54bedf793
Wheezy123 <JohnsonJeromeC@gmail.com>
2018-09-03 17:58:37 -0500

    passenger dash service now calls local db via http get, import HTTP and Response, add injectable, import rxjs/map to return http response

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,46 +1,25 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 
 import { Passenger } from './models/passenger.interface';
 
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+const PASSENGER_API: string = '/api/passengers';
+
+// Injectable needs to be used anytime an external dependency is imported!!
 @Injectable()
 export class PassengerDashboardService {
   constructor(private http: Http) {
     // console.log(this.http);
   }
 
-  getPassengers(): Passenger[]{
-    return [
-      {
-        id: 1,
-        name: 'jerome',
-        checkedIn: true
-      },
-      {
-        id: 2,
-        name: 'jessica',
-        checkedIn: false
-      },
-      {
-        id: 3,
-        name: 'tammy',
-        checkedIn: true
-      },
-      {
-        id: 4,
-        name: 'benny',
-        checkedIn: false
-      },
-      {
-        id: 5,
-        name: 'Jason',
-        checkedIn: true
-      },
-      {
-        id: 6,
-        name: 'molly',
-        checkedIn: true
-      }
-    ]
+  getPassengers(): Observable<Passenger[]>{
+    return this.http
+      .get(PASSENGER_API)
+      .map((response: Response) => {
+        return response.json();
+      });
   }
 }
