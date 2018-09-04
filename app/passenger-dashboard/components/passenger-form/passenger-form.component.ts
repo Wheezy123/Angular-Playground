85307b6791efdf3d3013218a5562932aaa75cce7
Wheezy123 <JohnsonJeromeC@gmail.com>
2018-09-03 23:13:19 -0500

    include Baggage interface in passenger form, add select option input for baggage, add example using ngValue also

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
-import { Passenger } from '../../models/passenger.interface'
+import { Passenger } from '../../models/passenger.interface';
+import { Baggage } from '../../models/baggage.interface';
+
 
 @Component({
   selector: 'passenger-form',
@@ -57,6 +59,31 @@ import { Passenger } from '../../models/passenger.interface'
             [ngModel]="detail?.checkInDate">
       </div>
 
+      <div>
+        Luggage:
+        <select
+          name="baggage"
+          [ngModel]="detail?.baggage">
+          <option
+            *ngFor="let item of baggage"
+            [value]="item.key"
+            [selected]="item.key === detail?.baggage">
+            {{item.value}}
+          </option>
+        </select>
+        <select
+          name="baggage"
+          [ngModel]="detail?.baggage">
+          <option
+            *ngFor="let item of baggage"
+            [ngValue]="item.key">
+            {{item.value}}
+          </option>
+        </select>
+      </div>
+
+      
+
 
       {{form.value | json }}
     </form>
@@ -67,6 +94,24 @@ import { Passenger } from '../../models/passenger.interface'
 export class PassengerFormComponent {
   @Input()
   detail: Passenger;
+
+  baggage: Baggage[] = [{
+      key: 'none',
+      value: 'No baggage'
+    },
+    {
+      key: 'hand-only',
+      value: 'Hand baggage'
+    },
+    {
+      key: 'hold-only',
+      value: 'Hold baggage'
+    },
+    {
+      key: 'hand-hold',
+      value: 'Hand and Hold baggage'
+    }
+  ];
 
   toggleCheckIn(checkedIn: boolean){
     if(checkedIn){
