11dee05534601c874f7c54f026fbeea92acb7c3e
Wheezy123 <JohnsonJeromeC@gmail.com>
2018-09-03 19:43:53 -0500

    include passenger viewer component

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,9 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
+  // template: `
+  //   <div class="app">
+  //     <passenger-dashboard></passenger-dashboard>
+  //   </div>    
+  // `
   template: `
     <div class="app">
-      <passenger-dashboard></passenger-dashboard>
+      <passenger-viewer></passenger-viewer>
     </div>    
   `
   // TEMPLATEURL is optional or could use above inline template
