92d773010c5bbe333842deeb0484a50e67d0b5c3
Wheezy123 <JohnsonJeromeC@gmail.com>
2018-09-04 00:07:46 -0500

    import Router, add home route, add HomeComponent

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,21 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
+import { RouterModule, Routes } from '@angular/router';
 
+import { HomeComponent } from './home.component';
 import { AppComponent } from './app.component';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   // *** Components are Declared, Modules are Imported!!!
   declarations: [
     // List declared Components from above here
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     // List declared Modules from above here
     // angular modules
     BrowserModule,
     CommonModule,
+    RouterModule.forRoot(routes),
     // custom modules
     PassengerDashboardModule
   ],
