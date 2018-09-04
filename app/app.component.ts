4248626d11491b670d5d75670c952f4248afc172
Wheezy123 <JohnsonJeromeC@gmail.com>
2018-09-04 00:32:58 -0500

    change to using dynamically generated routes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Nav {
+  link: string,
+  name: string,
+  exact: boolean
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
@@ -12,15 +18,11 @@ import { Component } from '@angular/core';
     <div class="app">
       <nav class="nav">
         <a 
-          routerLink="/"
-          routerLinkActive="active
-          [routerLinkActiveOptions]="{ exact: true }">
-          Home
-        </a>
-        <a 
-          routerLink="/oops"
-          routerLinkActive="active">
-          404
+          *ngFor="let item of nav"
+          [routerLink]="item.link"
+          routerLinkActive="active"
+          [routerLinkActiveOptions]="{ exact: item.exact }">
+          {{ item.name }}
         </a>
       </nav>
       <router-outlet></router-outlet>
@@ -30,5 +32,16 @@ import { Component } from '@angular/core';
   // templateUrl: './app.component.html'
 })
 export class AppComponent {
-  
+  nav: Nav[] = [
+    {
+      link: '/',
+      name: 'Home',
+      exact: true
+    },
+    {
+      link: '/opps',
+      name: '404',
+      exact: false
+    }
+  ];
 }
