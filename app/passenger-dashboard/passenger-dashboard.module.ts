5c9210a80eabc18d1d272b30ee9fa7b3350b0495
Wheezy123 <JohnsonJeromeC@gmail.com>
2018-09-04 00:42:44 -0500

    import RouterModule and Routes to passenger dashboard module, add Routes const definition, remove exports since they are no longer needed b/c of routing (forChild)

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 // Containers
 import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
@@ -16,6 +17,12 @@ import { PassengerFormComponent } from './components/passenger-form/passenger-fo
 // Services
 import { PassengerDashboardService } from './passenger-dashboard.service';
 
+const routes: Routes = [
+  {
+    path: 'passengers',
+    component: PassengerDashboardComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -28,11 +35,12 @@ import { PassengerDashboardService } from './passenger-dashboard.service';
   imports: [
     CommonModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forChild(routes)
   ],
   exports: [
     // PassengerDashboardComponent
-    PassengerViewerComponent
+    // PassengerViewerComponent
   ],
   providers: [
     PassengerDashboardService
